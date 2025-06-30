31a60a5f63f9dcf50d4cf7a48c223d8a2f241dff
YuriyNik156 <yuriy.nik156@gmail.com>
2025-06-30 20:22:23 +0300

    castomise users

--- auth_test/main/admin.py
+++ auth_test/main/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'phone', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    ordering = ('email',)
+    search_fields = ('email', 'phone')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Контактные данные', {'fields': ('phone',)}),
+        ('Права доступа', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Даты', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'phone', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
